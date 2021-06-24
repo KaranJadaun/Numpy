816850695452f8a21f3f7039a84409e43a073388
Karan Jadaun <jadaunfamily6@gmail.com>
2021-06-24 09:15:38 +0530

    Create DataTypes.py

---
+++ DataTypes.py
@@ -0,0 +1,32 @@
+# Importing numpy library
+import numpy as np
+
+print(np.int64(22))
+# Signed 64-bit integer types 
+# Output : 22
+
+print(np.float32(22))
+# Standard double-precision floating point
+# Output : 22.0
+
+print(np.complex(22))
+# Complex number represented by 128 floats
+# Output : (22+0j)
+
+print(np.bool(22))
+# Boolean type storing True and False values
+# Output : True
+
+print(np.object())
+# Python object type
+# Output : <object object ar 0x7f740458f0b0>
+
+print(np.string_('karan'))
+# Fixed-length string type
+# Output : b'karan'
+
+print(np.unicode_('k'))
+# Fixed-length unicode type
+# Output : k
+
+# Thank you
