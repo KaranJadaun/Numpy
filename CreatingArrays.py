bb050304ab726a43ffbbedbe34d8b7e965fc40ac
Karan Jadaun <jadaunfamily6@gmail.com>
2021-06-24 09:23:37 +0530

    Update CreatingArrays.py

--- CreatingArrays.py
+++ CreatingArrays.py
@@ -1,20 +1,25 @@
-# Import numpy library
+# Importing numpy library
 import numpy as np
 
 # Creating a 1D array
-a = np.array([1,2,3])
+print(np.array([1,2,3]))
+# Output : [1 2 3]
 
 # Creating a 2D array 
 # dtype is used to define the data type of the numpy array
-b = np.array([(1,2,3),(4,5,6)],dtype = int)
+print(np.array([(1,2,3),(4,5,6)],dtype = int))
+# Output : [[1 2 3] 
+#           [4 5 6]]
 
 # Creating a 3D array
 # dtype for defining that the array is in float
-c = np.array([[(1,2,3),(4,5,6),(7,8,9)],[(11,12,13),(14,15,16),(17,18,19)]],dtype=float)
-
-# Simply printing all the multi dimensional arrays
-print(a)
-print(b)
-print(c)
+print(np.array([[(1,2,3),(4,5,6),(7,8,9)],[(11,12,13),(14,15,16),(17,18,19)]],dtype=float))
+# Output : [[[1. 2. 3. ]
+#           [4. 5. 6. ]
+#           [7. 8. 9. ]] 
+#
+#           [[11. 12. 13. ]
+#            [14. 15. 16. ]
+#            [17. 18. 19. ]]]
 
 # Thank You
